bb72d233122f69f718a6cc72c5d221a6303d951f
Eduard Andreychev <ed_89@mail.ru>
2017-03-28 19:16:53 +0300

    added validation on focusOut 2

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -13,27 +13,61 @@ class Contact extends Component {
     }
 
     onChangeInput = (type, value) => {
-        this.props.changeInput(this.buildPayload(type, value, this.props.userData));
+        this.props.changeInput(this.buildPayload(type, value, this.props.userData));//dispatch
     };
 
     validateInput = (type, pattern) => {
-        //console.log(pattern);
-        //console.log(this.props.userData[type]);
+        if (this.props.userData[type].length) {//if field is not empty, we validate this field
+            if (!pattern.test(this.props.userData[type])) {
 
-        if (!this.props.userData[type].length) {
-            console.log('field - ' + type + ': is empty');
+                //console.log('field - ' + type + ': is NOT valid');
 
-            this.props.validateInput(this.buildPayload(type + 'IsValid', false, this.props.errors));
-        } else if (!pattern.test(this.props.userData[type])) {
-            console.log('field - ' + type + ': is NOT valid');
+                this.props.validateInput(this.buildPayload(type + 'IsValid', false, this.props.errors));//dispatch
+            } else {
 
-            this.props.validateInput(this.buildPayload(type + 'IsValid', false, this.props.errors));
-        } else {
-            console.log('All right!');
+                //console.log('All right!');
 
-            this.props.validateInput(this.buildPayload(type + 'IsValid', true, this.props.errors));
+                this.props.validateInput(this.buildPayload(type + 'IsValid', true, this.props.errors));//dispatch
+            }
+        }
+    };
+
+    validateForm = () => {
+        let emptyFields = {},
+            payload = {},
+            errorsCounter = 0;
+
+        for (let field in this.props.userData) {//we are checking each field on empty
+            if (!this.props.userData[field].length) {
+
+                //console.log('field "' + field + '" is empty');
+
+                emptyFields[field + 'IsValid'] = false;
+            }
+        }
+        payload = {...this.props.errors, ...emptyFields};
+        this.props.validateInput(payload);//dispatch empty fields
+
+        /*for (let error in this.props.errors) {
+            console.log(this.props.errors[error]);
+            if(this.props.errors[error]) {
+                errorsCounter+=1;
+            }
         }
 
+        console.log(errorsCounter);
+
+        if (!errorsCounter) {
+            console.log('form can bee submit');
+        } else {
+            console.log('form can NOT bee submit');
+        }*/
+    };
+
+    submitForm = (e) => {
+        e.preventDefault();
+
+        this.validateForm();
     };
 
     buildPayload = (type, value, state) => {
@@ -88,10 +122,12 @@ class Contact extends Component {
                         <div className="b-form__controls">
                             <button
                                 className="b-form__btn"
+                                onClick={this.submitForm}
                             >Sign Up</button>
                         </div>
                     </form>
                 </div>
+
             </div>
         )
     }
